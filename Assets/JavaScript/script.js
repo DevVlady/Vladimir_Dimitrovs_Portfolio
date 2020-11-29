3cc5a9359987d1346dfeb2075ef3a800283ebd15
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-28 23:19:33 -0600

    Added the Portfolio section in the html file with some images and styling.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,24 +1,43 @@
-//Obtain elements from HTML
-const mainPage = document.querySelector('#main-page')
-const homeButton = document.querySelector('#home-button')
-const aboutMePage = document.querySelector('#about-me-page')
-const viewMyWorkButton = document.querySelector('#view-my-work-button')
+//Obtain Page Sections from HTML
+const mainPage = document.querySelector('#main-page');
+const aboutMePage = document.querySelector('#about-me-page');
+const portfolioPage = document.querySelector('#portfolio-page');
 
 
-//Event listener to view my work button
-viewMyWorkButton.addEventListener('click', displayAboutPage)
+//Obtain Buttons/Links from HTML
+const homeButton = document.querySelector('#home-button');
+const viewMyWorkButton = document.querySelector('#view-my-work-button');
+const aboutMeButton = document.querySelector('#about-button');
+const portfolioButton = document.querySelector('#portfolio-button');
+
 
 //Event Listener for Home Buttom
-homeButton.addEventListener('click', displayMainPage)
+homeButton.addEventListener('click', displayMainPage);
+//Event listener to display the About Me page when about button is clicked
+aboutMeButton.addEventListener('click', displayAboutPage);
+//Event listener to display portfolio page when view my work btn is clicked
+viewMyWorkButton.addEventListener('click', displayPortfolioPage);
+//Event Listener to display portfolio page when port button is clicked
+portfolioButton.addEventListener('click', displayPortfolioPage);
+
 
 //Function to display the Main Page
 function displayMainPage () {
     mainPage.style.display = 'block';
     aboutMePage.style.display = 'none';
+    portfolioPage.style.display = 'none';
 }
 
 //Function to display the About Page
 function displayAboutPage () {
     mainPage.style.display = 'none';
     aboutMePage.style.display = 'block';
+    portfolioPage.style.display = 'none';
+}
+
+//Function to display Portfolio Page
+function displayPortfolioPage () {
+    mainPage.style.display = 'none';
+    aboutMePage.style.display = 'none';
+    portfolioPage.style.display = 'block';
 }
