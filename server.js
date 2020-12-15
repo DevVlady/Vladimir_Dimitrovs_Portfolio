4bd7d56ed779f90600f2f39409593f95bd02c8a1
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-15 14:31:42 -0600

    Created the the server function to start the server.

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 //Requiring neccessary variables to start server
 const express = require('express');
+const { appendFile } = require('fs');
 const app = express();
 
+//Creating the PORT
+const PORT = process.env.PORT || 8080;
+
+//Used for pulc folder route
+app.use(express.static('public'));
+
+//Starting Server
+app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port PORT: " + PORT);
+  });
