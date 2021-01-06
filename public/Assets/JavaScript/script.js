9d42c6ada49384304f35cf3eb125a059ba42f7d2
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-06 11:19:46 -0600

    Added the second card to my port page to display my projects.

--- public/Assets/JavaScript/script.js
+++ public/Assets/JavaScript/script.js
@@ -7,39 +7,8 @@ const contactMePage = document.querySelector('#contact-me-page');
 //Obtain the View My Work Button from home page section in HTML
 const viewMyWorkButton = document.querySelector('#view-my-work-button');
 
-//Get links from the About Me Page Section in HTML
-const ampHomeButton = document.querySelector('#amp-home-link');
-const ampPortfolioButton = document.querySelector('#amp-portfolio-link');
-const ampContactMeButton = document.querySelector('#amp-contact-link')
-
-//Get links from the Portfolio Page Section in HTML
-const ppHomeButton = document.querySelector('#pp-home-link');
-const ppAboutMeButton = document.querySelector('#pp-about-link');
-const ppContactMeButton = document.querySelector('#pp-contact-link');
-
-//Get links from the Contact Me Page section in HTML
-const cmpHomeButton = document.querySelector('#cmp-home-link');
-const cmpAboutMeButton = document.querySelector('#cmp-about-link');
-const cmpPortfolioButton = document.querySelector('#cmp-portfolio-link');
-
-
 //Event listeners for the View My Work Button on Home Page
 viewMyWorkButton.addEventListener('click', displayAboutPage);
-
-//Event Listeners for links on the About Me Page
-ampHomeButton.addEventListener('click', displayHomePage);
-ampPortfolioButton.addEventListener('click', displayPortfolioPage);
-ampContactMeButton.addEventListener('click', displayContactPage);
-
-//Event Listeners for all links in the Portfolio Page
-ppHomeButton.addEventListener('click', displayHomePage)
-ppAboutMeButton.addEventListener('click', displayAboutPage);
-ppContactMeButton.addEventListener('click', displayContactPage);
-
-//Event listeners for all links in the navbar on Contact Me Page
-cmpHomeButton.addEventListener('click', displayHomePage);
-cmpAboutMeButton.addEventListener('click', displayAboutPage);
-cmpPortfolioButton.addEventListener('click', displayPortfolioPage);
 
 
 //Function to display the Main Page
